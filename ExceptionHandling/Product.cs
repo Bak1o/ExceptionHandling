0413a99d7fcb9be40186dee306c3dd9df2632227
Bak1o <b1a2k3o4@gmail.com>
2024-11-08 16:12:39 +0400

    Add project files.

---
+++ ExceptionHandling/Product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ExceptionHandling.Program;
+
+namespace ExceptionHandling
+{
+    internal class Product
+    {
+        private int id;
+
+        public int Id { get => id; set => id = value; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public int Rating { get; set; }
+
+        public Product()
+        {
+            
+        }
+        public Product(int id, decimal price, string name, string manufacturer, int rating)
+        {
+            Id = id;
+            Price = price;
+            Name = name;
+            Manufacturer = manufacturer;
+            Rating = rating;
+        }
+
+        public bool Validate()
+        {
+            try
+            {
+                Ensure.IsPositive(Id);
+                Ensure.IsPositive(Price);
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(" The Id and Price value must be positive number ");
+            }
+            
+        }
+    }
+}
---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq.Expressions;
+using System.Net.NetworkInformation;
+using System.Numerics;
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        public static class Ensure
+        {
+            public static void Main(string[] args)
+            {
+               var product = new Product();
+                product.Price = -100;
+                product.Validate();
+
+            }
+            public static bool IsPositive<T>(T number)
+                  where T : INumberBase<T>
+            { if(T.IsPositive(number))
+                return true;
+                throw new Exception();
+            }
+            public static bool IsNegative<T>(T number)
+                 where T : INumberBase<T>
+            {
+                return T.IsNegative(number);
+            }
+            public static bool IsNonPositive<T>(T number)
+                where T : INumberBase<T>
+            {
+                return T.IsNegative(number);
+            }
+            public static bool IsNonNegative<T>(T number)
+              where T : INumberBase<T>
+            {
+                return T.IsPositive(number);
+            }
+        }
+        
+    }
+
+
+   //public static class Ensure
+   // {
+   //   public  static bool IsPositive<T>(T number)
+   //         where T : INumberBase<T>
+   //     {
+   //         return T.IsPositive(number);
+   //     }
+   //    public static bool IsNegative<T>(T number)
+   //         where T : INumberBase<T>
+   //     {
+   //         return T.IsNegative(number);
+   //     }
+   //    public static bool IsNonPositive<T>(T number)
+   //        where T : INumberBase<T>
+   //     {
+   //         return T.IsNegative(number);
+   //     }
+   //     public static bool IsNonNegative<T>(T number)
+   //       where T : INumberBase<T>
+   //     {
+   //         return T.IsPositive(number);
+   //     }
+   // }
+
+
+ 
+}
